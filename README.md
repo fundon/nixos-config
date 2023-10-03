3ebc5fd57c6ead05bfa50439a44f9a542a190040
Fangdun Tsai <cfddream@gmail.com>
2023-10-03 16:46:06 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# NixOS configurations
+
+
+Forked from [mitchellh's nixos-config](https://github.com/mitchellh/nixos-config).
---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  description = "NixOS systems and tools by fundon";
+
+  inputs = {
+    # Pin our primary nixpkgs repository. This is the main nixpkgs repository
+    # we'll use for our configurations. Be very careful changing this because
+    # it'll impact your entire system.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
+    # We use the unstable nixpkgs repo for some packages.
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # Build a custom WSL installer
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # I think technically you're not supposed to override the nixpkgs
+    # used by neovim but recently I had failures if I didn't pin to my
+    # own. We can always try to remove that anytime.
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Other packages
+    zig.url = "github:mitchellh/zig-overlay";
+  };
+
+  outputs = { self, nixpkgs, home-manager, darwin, ... }@inputs: let
+    # Overlays is the list of overlays we want to apply from flake inputs.
+    overlays = [
+      inputs.neovim-nightly-overlay.overlay
+      inputs.zig.overlays.default
+    ];
+
+    mkSystem = import ./lib/mksystem.nix {
+      inherit overlays nixpkgs inputs;
+    };
+  in {
+    nixosConfigurations.vm-aarch64 = mkSystem "vm-aarch64" {
+      system = "aarch64-linux";
+      user   = "fundon";
+    };
+
+    nixosConfigurations.vm-intel = mkSystem "vm-intel" rec {
+      system = "x86_64-linux";
+      user   = "fundon";
+    };
+
+    nixosConfigurations.wsl = mkSystem "wsl" {
+      system = "x86_64-linux";
+      user   = "fundon";
+      wsl    = true;
+    };
+
+    darwinConfigurations.macbook-pro-m1 = mkSystem "macbook-pro-m1" {
+      system = "aarch64-darwin";
+      user   = "fundon";
+      darwin = true;
+    };
+  };
+}
