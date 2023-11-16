af8dcbf19df19480cb06d8d7dd73c5627676c9b3
Fangdun Tsai <cfddream@gmail.com>
2023-11-16 18:40:12 +0800

    programs(git): extra config

--- programs/git.nix
+++ programs/git.nix
@@ -18,6 +18,9 @@
     };
 
     extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
       merge = {
         conflictstyle = "diff3";
       };
@@ -29,6 +32,23 @@
       };
       sequence = {
         editor = "${pkgs.git-interactive-rebase-tool}/bin/git-interactive-rebase-tool";
+      };
+      pretty = {
+        custom = "%C(magenta)%h%C(red)%d %C(yellow)%ar %C(green)%s %C(yellow)(%an)";
+        #                     │        │            │            │             └─ author name
+        #                     │        │            │            └─ message
+        #                     │        │            └─ date (relative)
+        #                     │        └─ decorations (branch, heads or tags)
+        #                     └─ hash (abbreviated)
+      };
+      aliases = {
+        graph = "log --graph --pretty=custom";
+
+        # List the latest 20 commits
+        l = "log -n 20 --graph --abbrev-commit --pretty=custom";
+
+        # List contributors
+        lc = "shortlog --email --summary --numbered";
       };
     };
   };
