b76e2a7663470e9883174bb676ad488f947f2f2f
Fangdun Tsai <fundon@pindash.io>
2023-11-25 18:23:56 +0800

    fix(scripts): sort profiles

--- scripts/clean-old-system-profiles.fish
+++ scripts/clean-old-system-profiles.fish
@@ -5,7 +5,7 @@ set profiles $prefix/system-*-link
 set profile $prefix/(readlink $prefix/system)
 
 echo "$profile is current profile"
-for i in (seq (count $profiles) | sort -R)
+for i in (seq (count $profiles) | sort -V)
     if test $profiles[$i] != $profile
         sudo rm -i $profiles[$i]
     end
